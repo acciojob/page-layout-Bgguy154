66da271de6080afe150cca53830bfd0fa63ecbe3
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2026-01-29 11:41:27 +0530

    Refactor PageLayout component usage for readability

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-        <PageLayout header={<h1>Welcome to my website</h1>} footer={<p>Ⓒ 2023 My Website .All rights reserved .</p>} children={<p>This is the Content of my website .</p>}/>
+      {/* Do not remove the main div */}
+      <PageLayout
+        header={<h1>Welcome to my website</h1>}
+        footer={<p>Ⓒ 2023 My Website .All rights reserved .</p>}
+      >
+              <p>This is the Content of my website .</p>
+    </PageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
